c1ff5ac0f3f36e1c844237fc0ce9e995678ee337
icebox <albertosantini@gmail.com>
2013-08-28 21:21:07 +0200

    Review the second example.

--- examples/ex2.js
+++ examples/ex2.js
@@ -3,12 +3,12 @@
 var opencpu = require("../lib/opencpu");
 
 opencpu.rCall("/library/stats/R/rnorm/json", {
-    n: 3,
+    n: 42,
     mean: 10,
     sd: 10
 }, function (err, data) {
     if (!err) {
-        console.log(data);
+        console.log(data.length); // => 42
     } else {
         console.log("opencpu call failed.");
     }
